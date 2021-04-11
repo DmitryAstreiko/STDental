b450f81f5193769f0a15d545560417c86718ce5b
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2021-04-12 02:14:37 +0300

    STDentalLibrary: added one migration for table Options

--- STDentalSolution/src/STDentalLibrary/Context/STDentalContext.cs
+++ STDentalSolution/src/STDentalLibrary/Context/STDentalContext.cs
@@ -7,7 +7,7 @@ namespace STDentalLibrary.Context
     {
         private readonly string _connectionValue;
 
-        public DbSet<Option> Options { get; set; }
+        //public DbSet<Option> Options { get; set; }
 
         public STDentalContext(string connectionValue = null)
         {
@@ -18,6 +18,32 @@ namespace STDentalLibrary.Context
         {
             optionsBuilder.UseSqlServer(
                 _connectionValue ?? @"Data Source=.\SQLExpress;Initial Catalog=STDentalTest;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Option>()
+                .ToTable("Options");
+
+            modelBuilder.Entity<Option>(option =>
+            {
+                option.HasKey(o => o.OptionsId);
+
+                option.Property(p => p.Name)
+                    .HasColumnName("Name")
+                    .HasColumnType("nvarchar(32)")
+                    .IsRequired();
+
+                option.Property(p => p.Value)
+                    .HasColumnName("Value")
+                    .HasColumnType("nvarchar(128)")
+                    .IsRequired();
+
+                option.Property(p => p.Description)
+                    .HasColumnName("Description")
+                    .HasColumnType("nvarchar(256)")
+                    .IsRequired();
+            });
         }
     }
 }
---
+++ STDentalSolution/src/STDentalLibrary/Migrations/20210411230748_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace STDentalLibrary.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Options",
+                columns: table => new
+                {
+                    OptionsId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(32)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(128)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(256)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Options", x => x.OptionsId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Options");
+        }
+    }
+}
