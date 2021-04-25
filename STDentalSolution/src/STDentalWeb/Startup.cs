4f45f1898c0d2d441b0c79488a8f99b7ff5253c1
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2021-04-26 01:07:46 +0300

    Rename project with STDentalWeb in STDentalWebEmpty

--- STDentalSolution/src/STDentalWeb/Startup.cs
+++ STDentalSolution/src/STDentalWeb/Startup.cs
@@ -1,22 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 using STDentalLibrary.Implementation;
 using STDentalLibrary.Models;
 using STDentalLibrary.Repositories;
 
-namespace STDentalWeb
+namespace STDentalWebEmpty
 {
     public class Startup
     {
