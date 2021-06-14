d248d30b05b79bef84a9b21e17c22b361f8232cf
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2021-06-14 16:09:00 +0300

    React: some changes in page Home

--- STDentalSolution/src/STDentalReact/ClientApp/src/Home.js
+++ STDentalSolution/src/STDentalReact/ClientApp/src/Home.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import functionality from './Images/functionality.png';
 import aboutSystem from './Images/aboutsystem.png';
 import softEnv from './Images/softwareenvironment.png';
+import roleAdministrator from './Images/roleadministrator.png';
+import roleHead from './Images/rolehead.png';
+import roleAccountant from './Images/roleaccountant.png';
+import roleDoctor from './Images/roledoctor.png';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -17,6 +21,18 @@ export class Home extends Component {
         </div>
         <div style={{display: "flex", alignItems: "center", flexDirection: 'column' }}>
           <img src={softEnv} alt="softEnv" style={{ width: "80%", margin: "20px" }}/>
+        </div>
+        <div style={{display: "flex", alignItems: "center", flexDirection: 'column' }}>
+          <img src={roleAdministrator} alt="roleAdministrator" style={{ width: "80%", margin: "20px" }}/>
+        </div>
+        <div style={{display: "flex", alignItems: "center", flexDirection: 'column' }}>
+          <img src={roleHead} alt="roleHead" style={{ width: "80%", margin: "20px" }}/>
+        </div>
+        <div style={{display: "flex", alignItems: "center", flexDirection: 'column' }}>
+          <img src={roleAccountant} alt="roleAccountant" style={{ width: "80%", margin: "20px" }}/>
+        </div>
+        <div style={{display: "flex", alignItems: "center", flexDirection: 'column' }}>
+          <img src={roleDoctor} alt="roleDoctor" style={{ width: "80%", margin: "20px" }}/>
         </div>
       </div>
     );
