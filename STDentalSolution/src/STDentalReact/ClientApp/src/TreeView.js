92b1168fa52ed266c339df8cfe624be2743856ed
DmitryAstreiko <70840183+DmitryAstreiko@users.noreply.github.com>
2021-07-27 01:20:03 +0300

    React: working TreeView services

--- STDentalSolution/src/STDentalReact/ClientApp/src/TreeView.js
+++ STDentalSolution/src/STDentalReact/ClientApp/src/TreeView.js
@@ -26,6 +26,78 @@ const data = {
   ],
 };
 
+const data2 = {
+
+  content: [
+    {
+      id: '2',
+      fullName: 'Child - 1',
+      shifr: '1.1',
+    },
+    {
+      id: '3',
+      fullName: 'Child - 3',
+      shifr: '1.2',
+      content: [
+        {
+          id: '7',
+          fullName: 'Child - 4',
+          shifr: '1.2.1',
+        },
+        {
+          id: '8',
+          fullName: 'Child - 4',
+          shifr: '1.2.2',
+        },
+      ],
+    },
+  ],
+ 
+};
+
+const tree = [{
+  text: '1 Ортопедия',
+  expanded: true,
+  items: [{
+    text: '1.1'
+  }, {
+    text: '1.2',
+    items : [
+      {
+        text: '1.2.1',
+        items: [
+          {
+            text: '1.2.1.1'
+          },
+          {
+            text: '1.2.1.2'
+          }
+        ],
+        text: '1.2.2',
+        items: [
+          {
+            text: '1.2.2.1'
+          },
+          {
+            text: '1.2.2.2'
+          }
+        ]
+      }
+    ]
+  }, {
+    text: 'Occasional Furniture'
+  }]
+}, {
+  text: '2 Зуботехнические',
+  items: [{
+    text: 'Bed Linen'
+  }, {
+    text: 'Curtains & Blinds'
+  }, {
+    text: 'Carpets'
+  }]
+}];
+
 const useStyles = makeStyles({
   root: {
     height: 110,
@@ -34,24 +106,69 @@ const useStyles = makeStyles({
   },
 });
 
-export default function RecursiveTreeView(props) {
+export default function TreeViewServices(props) {
   const classes = useStyles();
 
   const renderTree1 = (nodes) => (
     <TreeItem key={nodes.id} nodeId={nodes.id} label={nodes.name}>
-      {Array.isArray(nodes.children) ? nodes.children.map((node) => renderTree(node)) : null}
+      {Array.isArray(nodes.children) ? nodes.items.map((node) => renderTree(node)) : null}
     </TreeItem>
   );
 
-    const renderTree = (nodes) => (
-        nodes.map((node, index) => 
-            (node.shifr.length <= 2) && (
-                <TreeItem id={node.id} nodeId={node.id} label={node.fullName}>                    
-                </TreeItem>
-            )
+  const renderTree2 = (nodes) => (
+      nodes.map((node, index) => 
+          (node.shifr.length <= 2) && (
+              <TreeItem id={node.id} nodeId={node.id} label={node.fullName}>                    
+              </TreeItem>
+          )
+      )
+      
+  );
+
+  const renderTree4 = (nodes) => {
+    return nodes.map(node => {
+      let qqq = node.shifr;
+
+      if(node.shifr.length <= 2) {
+        return (
+          <TreeItem id={node.id} 
+            nodeId={node.id} 
+            label={node.fullName}></TreeItem>
         )
-        
-    );
+      }
+      else if(node.shifr.length <= 2) {
+        return (
+          <TreeItem id={node.id} 
+            nodeId={node.id} 
+            label={node.fullName}>
+              
+            </TreeItem>
+        )
+      }
+    })
+  }
+
+  const renderTree = (nodes) => {
+    return nodes.map(node => {
+      if(!node.items) {
+        return (
+          <TreeItem id={node.id} 
+            nodeId={node.id} 
+            label={node.fullName}></TreeItem>
+        );
+      }
+      else {
+        return (
+          <TreeItem id={node.id} 
+            nodeId={node.id} 
+            label={node.fullName}>
+              {renderTree(node.content)}
+            </TreeItem>
+        )
+      }
+    });
+  };
+    
 
   return (
     <TreeView
@@ -60,7 +177,8 @@ export default function RecursiveTreeView(props) {
       defaultExpanded={['root']}
       defaultExpandIcon={<ChevronRightIcon />}
     >
-      {renderTree(props.actualServices)}
+      {/*{renderTree(props.actualServices)}*/}
+      {renderTree1(tree)}
     </TreeView>
   );
 }
